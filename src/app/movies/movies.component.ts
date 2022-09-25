579b5ed12f3b3290b712906dbfcd65e1142a188d
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-09-25 11:33:51 +0300

    fix: pages params (not compleated)

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataService, MoviesData } from '../data.service';
+// import { HttpParams } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-movies',
@@ -11,17 +13,29 @@ export class MoviesComponent implements OnInit {
 
   imgPrefex: string = 'https://image.tmdb.org/t/p/w500/';
   apiKey: string = '?api_key=f82ecbb7a5110caecaee2bee5e4c79d6';
-  page: number = 1;
+  page: number | null = 1;
 
   constructor(
     public dataServace: DataService,
-    private router: Router
+    private router: Router,
+    // private activRoute: ActivatedRoute
+
   ) { }
 
   ngOnInit(): void {
-    this.dataServace.getMovies(this.page).subscribe((data: MoviesData) => {
+    // this.page = Number(this.activRoute.snapshot.paramMap.get('page'));
+    // if (this.page == null) {
+    //   const params = new HttpParams()
+    //     .set('page', 1);
+    //   this.router.navigate(['/movies', { params }]);
+    //   this.page = Number(this.activRoute.snapshot.paramMap.get('page'));
+    // }
+
+    this.dataServace.getMovies(Number(this.page)).subscribe((data: MoviesData) => {
       this.dataServace.moviesDataResults.next(data.results);
     })
+
+    this.router.navigate(['/movies', { page: this.page }]);
   }
 
   /**
@@ -29,15 +43,23 @@ export class MoviesComponent implements OnInit {
    * @param {number} pageNum The new page number
    */
   changePage(pageNum: number): void {
+    // const params = new HttpParams()
+    //   .set('page', pageNum);
+    // this.router.navigate(['/movies', { params }]);
+
+    // this.page = Number(this.activRoute.snapshot.paramMap.get('page'));
     this.page = pageNum
+
     this.dataServace.getMovies(this.page).subscribe((data: MoviesData) => {
       this.dataServace.moviesDataResults.next(data.results);
     })
+
+    this.router.navigate(['/movies', { page: this.page }]);
   }
 
   /**
    * Navigate to the details of the clicked movie
-   * @param {number} id the id of the movie
+   * @param {number} id The id of the movie
    */
   movieDetails(id: number): void {
     this.router.navigate(['/movie', id, { api_key: this.apiKey }]);
